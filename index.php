0d344c1c0cd20ae2d8066c4d87beaeb1041fe896
ProfVillegas <julian.villegas@utrivieramaya.edu.mx>
2017-08-14 13:51:33 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Ejemplo del uso de sesiones - </title>
+</head>
+
+<body>
+<h1>Ejemplo del uso de variables de sesión </h1>
+<ul>
+	<li><a href="without.php">Ejemplo de variable $contador con incremenento en 1</a></li>
+	<li><a href="with.php">Ejemplo de variable $_SESSION['contador'] con incremenento en 1</a></li>
+</ul>
+<hr>
+</body>
+</html>
